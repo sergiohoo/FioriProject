fdbf5a863856c1787d5350deab04d0d8eddad9fe
Sergio Ortiz <sergiohoo@gmail.com>
2021-04-21 23:00:58 +0000

    Lab15.1 terminado

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,14 +1,44 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-	function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
-		var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
-            
+        function onInit() {
+            var oJSONModel = new JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
+
+            var oJSON = {
+                employeeId: "123456",
+                countryKey: "UK",
+                listCountry: [
+                    {
+                        key: "US",
+                        text: i18nBundle.getText("countryUS")
+                    },
+                    {
+                        key: "UK",
+                        text: i18nBundle.getText("countryUK")
+                    },
+                    {
+                        key: "ES",
+                        text: i18nBundle.getText("countryES")
+                    }
+                ]
+            };
+
+            oJSONModel.setData(oJSON);
+            oView.setModel(oJSONModel);
+        }
+
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
+
         Main.prototype.onValidate = function () {
             var inputEmployee = this.getView().byId("inputEmployee");
             var valueEmployee = inputEmployee.getValue();
@@ -23,5 +53,7 @@ sap.ui.define([
                 this.getView().byId("slCountry").setVisible(false);
             };
         }
-		return Main;
-	});
+
+        Main.prototype.onInit = onInit;
+        return Main;
+    });
