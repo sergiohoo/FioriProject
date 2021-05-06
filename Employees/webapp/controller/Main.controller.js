756f99eceb8ce2858a87bac774e26ce08d14f584
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-06 21:28:00 +0000

    Laboratorio 22 terminado

--- Employees/webapp/controller/Main.controller.js
+++ Employees/webapp/controller/Main.controller.js
@@ -44,6 +44,10 @@ sap.ui.define([
                 var detailView = this.getView().byId("detailEmployeeView");
                 detailView.bindElement("jsonEmployees>" + path);
                 this.getView().getModel("jsonLayout").setProperty("/ActiveKey", "TwoColumnsMidExpanded");
+
+                var incidenceModel = new sap.ui.model.json.JSONModel([]);
+                detailView.setModel(incidenceModel,"incidenceModel");
+                detailView.byId("tableIncidence").removeAllContent();
             }
         });
     });
