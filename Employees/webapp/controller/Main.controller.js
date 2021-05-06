3b892eb0fb315492f07206dec1df4a10c7fcea83
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-06 15:14:02 +0000

    Lab21.2 terminado

--- Employees/webapp/controller/Main.controller.js
+++ Employees/webapp/controller/Main.controller.js
@@ -11,26 +11,29 @@ sap.ui.define([
 
             onInit: function () {
                 var oView = this.getView();
-            // var i18nBundle = oView.getModel("i18n").getResourceBundle();
+                // var i18nBundle = oView.getModel("i18n").getResourceBundle();
 
-            var oJSONModelEmployees = new sap.ui.model.json.JSONModel();
-            oJSONModelEmployees.loadData("./localService/mockdata/Employees.json", false);
-            oView.setModel(oJSONModelEmployees, "jsonEmployees");
+                var oJSONModelEmployees = new sap.ui.model.json.JSONModel();
+                oJSONModelEmployees.loadData("./localService/mockdata/Employees.json", false);
+                oView.setModel(oJSONModelEmployees, "jsonEmployees");
 
-            var oJSONModelCountries = new sap.ui.model.json.JSONModel();
-            oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
-            oView.setModel(oJSONModelCountries, "jsonCountries");
+                var oJSONModelCountries = new sap.ui.model.json.JSONModel();
+                oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
+                oView.setModel(oJSONModelCountries, "jsonCountries");
 
-            var oJSONModelConfig = new sap.ui.model.json.JSONModel({
-                visibleID: true,
-                visibleName: true,
-                visibleCountry: true,
-                visibleCity: false,
-                visibleBtnShowCity: true,
-                visibleBtnHideCity: false
-            });
-            oView.setModel(oJSONModelConfig, "jsonModelConfig");
+                var oJSONModelConfig = new sap.ui.model.json.JSONModel({
+                    visibleID: true,
+                    visibleName: true,
+                    visibleCountry: true,
+                    visibleCity: false,
+                    visibleBtnShowCity: true,
+                    visibleBtnHideCity: false
+                });
+                oView.setModel(oJSONModelConfig, "jsonModelConfig");
+
+                var oJSONModelLayout = new sap.ui.model.json.JSONModel();
+                oJSONModelLayout.loadData("./localService/mockdata/Layout.json", false);
+                oView.setModel(oJSONModelLayout, "jsonLayout");
             }
-
         });
     });
