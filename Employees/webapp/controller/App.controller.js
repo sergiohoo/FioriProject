5bede72a17248ddf9e1a437364cd344cfadfedaa
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-13 19:49:10 +0000

    Lab26.1 terminado

---
+++ Employees/webapp/controller/App.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller) {
+
+        return Controller.extend("logaligroup.Employees.controller.App", {
+
+            onInit: function () {
+
+            }
+        });
+    });
+
+
+    
