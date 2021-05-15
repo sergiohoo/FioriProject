b397077ef8ea6895bb2c6ddae07e251de54f46a8
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-15 17:03:15 +0000

    Lab26.4 terminado

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -1,16 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.core.routing.History} History 
      */
-    function (Controller) {
+    function (Controller, History) {
 
         return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
 
             onInit: function () {
 
+            },
+            onBack: function (oEvent) {
+                let oHistory = History.getInstance();
+                let sPreviousHash = oHistory.getPreviousHash();
+
+                if(sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("RouteMain", true);
+                }
             }
         });
     });
