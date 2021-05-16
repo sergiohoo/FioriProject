fdebc685e214fd923b65c891b40159d2a66260ea
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-16 20:44:16 +0000

    Laboratorio 29 terminado

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History"
@@ -26,7 +27,7 @@ sap.ui.define([
                 let oHistory = History.getInstance();
                 let sPreviousHash = oHistory.getPreviousHash();
 
-                if(sPreviousHash !== undefined) {
+                if (sPreviousHash !== undefined) {
                     window.history.go(-1);
                 } else {
                     let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
@@ -36,6 +37,42 @@ sap.ui.define([
             onClearSignature: function (oEvent) {
                 let signature = this.byId("signature");
                 signature.clear();
+            },
+
+            factoryOrderDetails: function (listId, oContext) {
+                let contextObject = oContext.getObject();
+                contextObject.Currency = "EUR";
+                let unitsInStock = oContext.getModel().getProperty("/Products(" + contextObject.ProductID + ")/UnitsInStock");
+
+                if (contextObject.Quantity <= unitsInStock) {
+                    let objectListItem = new sap.m.ObjectListItem({
+                        title: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})",
+                        number: "{parts: [" +
+                            "{path: 'odataNorthwind>UnitPrice'}, " +
+                            "{path: 'odataNorthwind>Currency'}], " +
+                            "type: 'sap.ui.model.type.Currency', " +
+                            "formatOptions: {showMeasure: false}}",
+                        numberUnit: "{odataNorthwind>Currency}"
+                    });
+                    return objectListItem;
+                }
+                else {
+                    let customListItem = new sap.m.CustomListItem({
+                        content: [
+                            new sap.m.Bar({
+                                contentLeft: new sap.m.Label({ text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})" }),
+                                contentMiddle: new sap.m.ObjectStatus({ text: "{i18n>availableStock} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state: "Error" }),
+                                contentRight: new sap.m.Label({
+                                    text: "{parts: [" +
+                                        "{path: 'odataNorthwind>UnitPrice'}, " +
+                                        "{path: 'odataNorthwind>Currency'}], " +
+                                        "type: 'sap.ui.model.type.Currency'}"
+                                })
+                            })
+                        ]
+                    });
+                    return customListItem;
+                }
             }
         });
     });
